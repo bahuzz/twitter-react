698d547a7da2b62e62aaca93373ed423579a53c3
bahuzz <dbahuzz@gmail.com>
2019-11-06 00:52:25 +0300

    added date to list item

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './post-list-item.css';
 
+
+
 const PostListItem = () => {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let postdate = `${day}-${month}-${year}`;
     return (
         <li className="app-list-item d-flex justify-content-between">
             <span className="app-list-item-label">
@@ -19,6 +26,7 @@ const PostListItem = () => {
                     <i className="fa fa-trash-o"></i>
                 </button>
                 <i className="fa fa-heart"></i>
+                <span className="app-list-item-date">{postdate}</span>
             </div>
         </li>
     )
